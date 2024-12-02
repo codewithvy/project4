7dc4c02a518f89c0f939252e144cde0d0bb9a911
Jose Olvera <joseolvera269@icloud.com>
2024-12-01 19:33:44 -0600

    edit

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Use SQLite's in-memory mode
 DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
