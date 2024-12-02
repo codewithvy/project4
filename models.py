b460401839d47296a8bb04ad18ab267403d54588
Jose Olvera <joseolvera269@icloud.com>
2024-12-02 13:30:33 -0600

    edits

--- models.py
+++ models.py
@@ -1,34 +1,3 @@
-'''from pydantic import BaseModel
-from sqlalchemy import Column, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
-from database import Base
-
-class CartItem(Base):
-    __tablename__ = "cart_items"
-    id = Column(Integer, primary_key=True, index=True)
-    product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
-    quantity = Column(Integer, nullable=False)
-    price = Column(Float, nullable=False)
-
-    # Relationship to the Product model
-    product = relationship("Product")
-
-
-# Product Model
-class Product(BaseModel):
-    id: int = None
-    name: str
-    description: str
-    price: float
-    inventory: int
-    category_id: int
-
-
-# Category Model
-class Category(BaseModel):
-    id: int = None
-    name: str
-'''
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
